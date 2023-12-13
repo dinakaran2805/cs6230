b315b5ef7d066ecd889e3993ad318a49ab7f9dc2
ssanthoshpavan <santhosh@mindgrovetech.in>
2023-12-13 22:20:41 +0530

    testbench

--- cfloat8_1_4_3/division/div_ref_model.py
+++ cfloat8_1_4_3/division/div_ref_model.py
@@ -46,8 +46,18 @@ def decimal_to_cfloat8_1_4_3(d):
     n = sign + exp + mant
     return n
 
+def denormal(op):
+    if op in [0.125, 00.250, 0.375, 0.5, 0.625, 0.750, 0.875]:
+        return 1
+    else:
+        return 0
+
 def division (op1, op2, bias):
+    
+    dn = denormal(op1) or denormal(op2) 
+
     q = op2/op1
+
     
     uf = 0
     guf = 0
@@ -75,7 +85,7 @@ def division (op1, op2, bias):
                     temp_q = (x+1)*0.125
                 break
             x += 1
-        uf = 1
+       # uf = 1
         guf = 1
         
     elif q > 0.875 and q < 2:
@@ -86,8 +96,8 @@ def division (op1, op2, bias):
             guf = 1
         else:
             temp_q = 2
-            unk = 1
-        uf = 1
+        unk = 1
+        #uf = 1
     
     elif q >= 2 and q <= (1.875 * (2**15)):
         num = q
@@ -112,23 +122,25 @@ def division (op1, op2, bias):
     elif q > (1.875 * (2**15)):
         temp_q = (1.875 * (2**15))
         of = 1
-            
-    return (temp_q * (2**bias))
 
-def main():
-    b = "00000010"
-    a = "00001000"
-    bias = 0
+    result = (temp_q * (2**bias))
+    status = '0'+str(dn)+str(of)+str(uf)+str(guf)+str(unk)+str(z)
+    return result, status
+
+def div_rm(a, b, bias):
+    #b = "00000010"
+    #a = "00001000"
+    #bias = 0
     
     op1 = cfloat8_1_4_3_to_decimal(a)
     op2 = cfloat8_1_4_3_to_decimal(b)
     #print(op1 , op2)
-    quo = division (abs(op1), abs(op2), bias)
+    quo, status = division (abs(op1), abs(op2), bias)
     #print(quo)
     if op1*op2 > 0:
         q = quo
     elif op1*op2 < 0:
         q = -quo
-    res = decimal_to_cfloat8_1_4_3(q)
-    return res
-print(main())
+    result = decimal_to_cfloat8_1_4_3(q)
+    return result, status
+#print(main())
